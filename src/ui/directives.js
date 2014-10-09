8e0eb6974f120ddc2e49235ef1e1145023dbe5a9
CrisFeo <feo.cris@gmail.com>
2014-10-09 08:33:30 -0500

    Add doc comment to file input directive.

--- src/ui/directives.js
+++ src/ui/directives.js
@@ -3,6 +3,16 @@ var R = require('ramda');
 var remote = require('remote');
 var dialog = remote.require('dialog');
 
+/**
+ * This directive represents a file input control. Create it with the
+ * <file-input> tag.
+ *
+ * label:          The label to display above the input control.
+ * filename:       A bound variable storing the currently selected filename
+ *                  or an array of filenames if multi-select is enabled.
+ * dialogOptions:  The options object which will be passed to atom-shell's
+ *                  'dialog.showOpenDialog' method. Check the docs for deets.
+ **/
 app.directive('fileInput', function () {
   return {
     'restrict': "E",
